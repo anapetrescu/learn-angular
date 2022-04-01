4df377a6c8311094e7c54f36565d94d275ee4ccc
Petrescu Ana-Maria <32705011+anapetrescu@users.noreply.github.com>
2022-04-01 19:53:47 +0300

    Lesson 1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+
+
+const routes: Routes = [
+  {path: '', component: ParentComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
